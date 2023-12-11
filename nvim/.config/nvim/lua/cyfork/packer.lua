f0d86ed7e49dbb31ff91104624a1d231196317c6
Marijan Batarilo <marijan.batarilo@circuitmess.com>
2023-12-11 10:37:50 +0100

    add: refactoring and trouble

--- nvim/.config/nvim/lua/cyfork/packer.lua
+++ nvim/.config/nvim/lua/cyfork/packer.lua
@@ -2,11 +2,12 @@ return require('packer').startup(function(use)
     use'wbthomason/packer.nvim'
 
     use {
-      'nvim-telescope/telescope.nvim', tag = '0.1.4',
-      -- or                          , branch = '0.1.x',
-      requires = { {'nvim-lua/plenary.nvim'} }
+        'nvim-telescope/telescope.nvim', tag = '0.1.4',
+        -- or                          , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
     }
     use'theprimeagen/harpoon'
+    use'theprimeagen/refactoring.nvim'
     use {
         'nvim-treesitter/nvim-treesitter',
         run = function()
@@ -19,37 +20,49 @@ return require('packer').startup(function(use)
     use'mbbill/undotree'
     -- quality pencils
     use({
-      'rose-pine/neovim',
-      as = 'rose-pine',
-      config = function()
-        vim.cmd('colorscheme rose-pine')
-      end
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        config = function()
+            vim.cmd('colorscheme rose-pine')
+        end
     })
     use'folke/tokyonight.nvim'
     use'chrisbra/Colorizer'
     -- lsd
     use {
-      'VonHeikemen/lsp-zero.nvim',
-      branch = 'v1.x',
-      requires = {
-        -- LSP Support
-        {'neovim/nvim-lspconfig'},
-        {'williamboman/mason.nvim'},
-        {'williamboman/mason-lspconfig.nvim'},
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
 
-        -- Autocompletion
-        {'hrsh7th/nvim-cmp'},
-        {'hrsh7th/cmp-buffer'},
-        {'hrsh7th/cmp-path'},
-        {'saadparwaiz1/cmp_luasnip'},
-        {'hrsh7th/cmp-nvim-lsp'},
-        {'hrsh7th/cmp-nvim-lua'},
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-buffer'},
+            {'hrsh7th/cmp-path'},
+            {'saadparwaiz1/cmp_luasnip'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'hrsh7th/cmp-nvim-lua'},
 
-        -- Snippets
-        {'L3MON4D3/LuaSnip'},
-        {'rafamadriz/friendly-snippets'},
-      }
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},
+            {'rafamadriz/friendly-snippets'},
+        }
     }
+    use({
+        "folke/trouble.nvim",
+        requires = { {'nvim-tree/nvim-web-devicons'} },
+        config = function()
+            require("trouble").setup {
+                icons = true,
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    })
     -- git
     use'tpope/vim-fugitive'
     -- comments
