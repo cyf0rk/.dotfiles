42fabdc6ea23c591975d3085f090c6dfbea88d53
cyf0rk <cyf0rk@pop-os.localdomain>
2024-08-08 21:20:07 +0200

    feat: notes

--- nvim/.config/nvim/after/plugin/obsidian.lua
+++ nvim/.config/nvim/after/plugin/obsidian.lua
@@ -7,7 +7,7 @@ obsidian.setup({
   workspaces = {
     {
       name = "notes",
-      path = "~/vaults/notes",
+      path = "~/vaults/Personal",
     },
   },
 
@@ -21,17 +21,6 @@ obsidian.setup({
   -- Optional, set the log level for obsidian.nvim. This is an integer corresponding to one of the log
   -- levels defined by "vim.log.levels.*".
   log_level = vim.log.levels.INFO,
-
-  daily_notes = {
-    -- Optional, if you keep daily notes in a separate directory.
-    folder = "notes/dailies",
-    -- Optional, if you want to change the date format for the ID of daily notes.
-    date_format = "%Y-%m-%d",
-    -- Optional, if you want to change the date format of the default alias of daily notes.
-    alias_format = "%B %-d, %Y",
-    -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-    template = nil
-  },
 
   -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
   completion = {
@@ -73,63 +62,16 @@ obsidian.setup({
     },
   },
 
-  -- Optional, customize how names/IDs for new notes are created.
-  note_id_func = function(title)
-    -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
-    -- In this case a note with the title 'My new note' will be given an ID that looks
-    -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
-    local suffix = ""
-    if title ~= nil then
-      -- If title is given, transform it into valid file name.
-      suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
-    else
-      -- If title is nil, just add 4 random uppercase letters to the suffix.
-      for _ = 1, 4 do
-        suffix = suffix .. string.char(math.random(65, 90))
-      end
-    end
-    return tostring(os.time()) .. "-" .. suffix
-  end,
-
-  -- Optional, customize the default name or prefix when pasting images via `:ObsidianPasteImg`.
-  image_name_func = function()
-    -- Prefix image names with timestamp.
-    return string.format("%s-", os.time())
-  end,
-
   -- Optional, boolean or a function that takes a filename and returns a boolean.
   -- `true` indicates that you don't want obsidian.nvim to manage frontmatter.
   disable_frontmatter = false,
-
-  -- Optional, alternatively you can customize the frontmatter data.
-  note_frontmatter_func = function(note)
-    -- This is equivalent to the default frontmatter function.
-    local out = { id = note.id, aliases = note.aliases, tags = note.tags }
-    -- `note.metadata` contains any manually added fields in the frontmatter.
-    -- So here we just make sure those fields are kept in the frontmatter.
-    if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
-      for k, v in pairs(note.metadata) do
-        out[k] = v
-      end
-    end
-    return out
-  end,
-
-  -- Optional, for templates (see below).
-  templates = {
-    subdir = "templates",
-    date_format = "%Y-%m-%d",
-    time_format = "%H:%M",
-    -- A map for custom variables, the key should be the variable and the value a function
-    substitutions = {},
-  },
 
   -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
   -- URL it will be ignored but you can customize this behavior here.
   follow_url_func = function(url)
     -- Open the URL in the default web browser.
-    vim.fn.jobstart({"open", url})  -- Mac OS
-    -- vim.fn.jobstart({"xdg-open", url})  -- linux
+    -- vim.fn.jobstart({"open", url})  -- Mac OS
+    vim.fn.jobstart({"xdg-open", url})  -- linux
   end,
 
   -- Optional, set to true if you use the Obsidian Advanced URI plugin.
