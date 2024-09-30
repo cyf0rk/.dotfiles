12d5ba14e4c95a6c8a6d33ca2e886c631d08b11e
Marijan Batarilo <marijan.batarilo@circuitmess.com>
2024-09-30 20:56:15 +0200

    feat: additional keymaps

--- nvim/.config/nvim/lua/plugins/codeium.lua
+++ nvim/.config/nvim/lua/plugins/codeium.lua
@@ -8,18 +8,19 @@ return {
         vim.keymap.set("i", "<C-x>", function()
             return vim.fn["codeium#Clear"]()
         end, { expr = true, silent = true, desc = "Codeium Clear" })
-        vim.keymap.set(
-            "i",
-            "<C-]>",
-            function()
-                return vim.fn["codeium#CycleCompletions"](1)
-            end,
-            {
-                expr = true,
-                silent = true,
-                desc = "Codeium Cycle Completions Next",
-            }
-        )
+        vim.keymap.set("i", "<C-]>", function()
+            return vim.fn["codeium#CycleCompletions"](1)
+        end, {
+            expr = true,
+            silent = true,
+            desc = "Codeium Cycle Completions Next",
+        })
+        vim.keymap.set("n", "<leader>cd", function()
+            vim.cmd.CodeiumDisable()
+        end, { expr = true, silent = true, desc = "Codeium Disable" })
+        vim.keymap.set("n", "<leader>ce", function()
+            vim.cmd.CodeiumEnable()
+        end, { expr = true, silent = true, desc = "Codeium Enable" })
 
         vim.g.codeium_filetypes = {
             markdown = false,
