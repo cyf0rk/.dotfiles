082f1437f4fa9e4b3605c0b95ec088d2cd129436
Marijan <marijanbatarilo.dev@gmail.com>
2026-07-23 12:06:08 +0200

    chore: change treesitter

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -3,6 +3,18 @@ return {
     tag = "0.1.4",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
+        -- telescope's preview highlighter still calls the legacy
+        -- nvim-treesitter.parsers/configs API removed by the nvim-treesitter
+        -- main branch rewrite; disable it so previews fall back to regex
+        -- highlighting instead of erroring on every non-file preview
+        require("telescope").setup({
+            defaults = {
+                preview = {
+                    treesitter = false,
+                },
+            },
+        })
+
         local builtin = require("telescope.builtin")
         vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
         vim.keymap.set("n", "<C-p>", builtin.git_files, {})
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,25 +1,54 @@
 return {
-  "romus204/tree-sitter-manager.nvim",
-  dependencies = {}, -- tree-sitter CLI must be installed system-wide
-  config = function()
-    require("tree-sitter-manager").setup({
-      -- Default Options
-      -- highlight = true, -- treesitter highlighting is enabled by default
-      -- languages = {}, -- override or add new parser sources
-      -- parser_dir = vim.fn.stdpath("data") .. "/site/parser",
-      -- query_dir = vim.fn.stdpath("data") .. "/site/queries",
-      ensure_installed = {
-          "markdown",
-          "markdown_inline",
-          "vimdoc",
-          "javascript",
-          "typescript",
-          "c",
-          "lua",
-          "python",
-          "go",
-      },
-      auto_install = true,
-    })
-  end
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    build = ":TSUpdate",
+    lazy = false,
+    config = function()
+        local ts = require("nvim-treesitter")
+        ts.setup({})
+        local ensure_installed = {
+            -- go
+            "go", "gomod", "gosum", "gowork",
+            -- python
+            "python",
+            -- php
+            "php", "phpdoc",
+            -- js/ts/react/vue/htmx (html covers htmx attributes)
+            "javascript", "typescript", "tsx", "vue", "html", "css",
+            -- rust
+            "rust",
+            -- bash
+            "bash",
+            -- data/config formats
+            "json", "yaml", "toml",
+            -- docs
+            "markdown", "markdown_inline",
+            -- misc / editing quality of life
+            "lua", "vim", "vimdoc", "query", "regex",
+            "dockerfile", "sql", "graphql",
+            "gitcommit", "gitignore", "diff",
+        }
+
+        local installed = ts.get_installed and ts.get_installed("parsers") or {}
+        local to_install = vim.tbl_filter(function(lang)
+            return not vim.list_contains(installed, lang)
+        end, ensure_installed)
+
+        if #to_install > 0 then
+            ts.install(to_install)
+        end
+
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "*",
+            callback = function(args)
+                local lang = vim.treesitter.language.get_lang(args.match) or args.match
+                if not pcall(vim.treesitter.start, args.buf, lang) then
+                    return
+                end
+
+                vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end,
+        })
+    end,
 }
